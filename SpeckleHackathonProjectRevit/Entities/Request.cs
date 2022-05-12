9b12c006c958391dbdb38fe89cb8ec82cc0d4f4e
antonrafael <anton.rafael10@gmail.com>
2022-05-12 16:25:32 -0300

    Domain project deleted

---
+++ SpeckleHackathonProjectRevit/Entities/Request.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+namespace SpeckleHackathonProjectRevit.Entities
+{
+    public class Request
+    {
+        [JsonProperty("field")]
+        public string Field { get; set; }
+
+        [JsonProperty("success")]
+        public string Success { get; set; }
+
+        [JsonProperty("element")]
+        public string Element { get; set; }
+
+        [JsonProperty("element_name")]
+        public string ElementName { get; set; }
+
+        [JsonProperty("direction")]
+        public string Direction { get; set; }
+
+        [JsonProperty("number")]
+        public string Number { get; set; }
+
+        [JsonProperty("unit")]
+        public string Unit { get; set; }
+
+        [JsonProperty("answer")]
+        public string Answer { get; set; }
+
+    }
+}
