b16e14c6b920da1fa6fe3c5e06ed8fa51fa49a49
carlos <carvillasil@gmail.com>
2022-05-12 16:21:37 +0200

    NLP understanding improved

--- nlp_agent.py
+++ nlp_agent.py
@@ -51,12 +51,36 @@ class NLPField(NLPModelsHelper):
                     return True
             return False
 
+    def which_element(self, prompt):
+        candidate = self.zero_shot_multiple(prompt, self.elements)
+        return candidate if self.zero_shot(prompt, candidate) else None
+
+    def element_name(self, prompt, element):
+        question = f'Which is the name of the {element}?'
+        name = self.question_answerer(question, prompt)
+        return None if name == '' else name.split()[0]
+
+    def action(self, prompt, element, element_name):
+        question = f'What should we do with the {element} {element_name}?'
+        return self.question_answerer(question, prompt)
+
+    def overall_direction(self, prompt):
+        question = f'To where?'
+        return self.question_answerer(question, prompt)
+
+    def direction(self, prompt):
+        return self.zero_shot_multiple(prompt, self.directions)
+
+    def amount(self, prompt):
+        question = f'By how much?'
+        return self.question_answerer(question, prompt)
+
     @staticmethod
     def amount_breakdown(amount):
         try:
             number = re.findall(r'(?<![a-zA-Z:])[-+]?\d*\.?\d+', amount)[0]
         except IndexError:
-            return None, None
+            return None
         unit = ''.join([letter for letter in amount if letter.isalnum() and not letter.isdigit()])
         unit = 'm' if unit == '' else unit  # defaulting to meters
         return float(number), unit
@@ -65,7 +89,7 @@ class NLPField(NLPModelsHelper):
         if parsed_items is None:
             txt = 'Sorry, '
         else:
-            txt = f'Hey! I understood you are referring to {self.summary(parsed_items)}. However, '
+            txt = f'Hey! I understood {self.summary(parsed_items)}. However, '
         txt += f'I could not understand which {item} you are talking about. '
         txt += f'Would you mind to rephrase it in a clear way for me and try again? Thanks in advance!'
         return txt
@@ -86,25 +110,6 @@ class NLPStructuralField(NLPField):
         self.elements = ['wall', 'slab', 'beam', 'column']
         self.directions = ['up', 'down', 'front', 'back', 'left', 'right']
 
-    def which_element(self, prompt):
-        candidate = self.zero_shot_multiple(prompt, self.elements)
-        return candidate if self.zero_shot(prompt, candidate) else None
-
-    def element_name(self, prompt, element):
-        question = f'Which is the name of the {element}?'
-        return self.question_answerer(question, prompt)
-
-    def action(self, prompt, element, element_name):
-        question = f'What should we do with the {element} {element_name}?'
-        return self.question_answerer(question, prompt)
-
-    def direction(self, prompt):
-        return self.zero_shot_multiple(prompt, self.directions)
-
-    def amount(self, prompt):
-        question = f'By how much?'
-        return self.question_answerer(question, prompt)
-
     @property
     def nlp_recipe(self):
         return OrderedDict({
@@ -120,9 +125,14 @@ class NLPStructuralField(NLPField):
                 'func': self.action,
                 'args': ['prompt', 'element', 'element_name']
             },
+            'overall_direction': {
+                'func': self.overall_direction,
+                'args': ['prompt'],
+                'readable_value_name': 'direction'
+            },
             'direction': {
                 'func': self.direction,
-                'args': 'action'
+                'args': 'overall_direction'
             },
             'amount': {
                 'func': self.amount,
@@ -130,13 +140,14 @@ class NLPStructuralField(NLPField):
             },
             'number_unit': {
                 'func': self.amount_breakdown,
-                'args': 'amount'
+                'args': 'amount',
+                'readable_value_name': 'amount'
             }
         })
 
     @property
     def parsed_items_to_discard(self):
-        return ['prompt', 'action', 'amount', 'number_unit']
+        return ['prompt', 'action', 'overall_direction', 'amount', 'number_unit']
 
     @staticmethod
     def summary(parsed_items):
@@ -158,7 +169,7 @@ class NLPStructuralField(NLPField):
     def on_success(self, parsed_items):
         return f'Hey! Perfect, I understood that {self.summary(parsed_items)}. Doing my job now!'
 
-    def process_one_step(self, value_name, parsed_items, func, args=None):
+    def process_one_step(self, value_name, parsed_items, func, args=None, readable_value_name=None):
         if args is None:
             args = []
         else:
@@ -166,7 +177,8 @@ class NLPStructuralField(NLPField):
             args = [parsed_items[arg] for arg in args]
         value = func(*args)
         if value is None:
-            return {**parsed_items, **{'answer': self.on_fail('element', parsed_items)}}
+            value_name = value_name if readable_value_name is None else readable_value_name
+            return {**parsed_items, **{'answer': self.on_fail(value_name, parsed_items)}}
         parsed_items[value_name] = value
         return parsed_items
 
@@ -175,7 +187,8 @@ class NLPStructuralField(NLPField):
         for step_name, step_data in self.nlp_recipe.items():
             func = step_data['func']
             args = step_data['args'] if 'args' in step_data else []
-            parsed_items = self.process_one_step(step_name, parsed_items, func, args)
+            readable_value_name = step_data['readable_value_name'] if 'readable_value_name' in step_data else None
+            parsed_items = self.process_one_step(step_name, parsed_items, func, args, readable_value_name)
             if step_name not in parsed_items:
                 return self.discard_items(parsed_items)
 
@@ -185,20 +198,21 @@ class NLPStructuralField(NLPField):
         return {**parsed_items, **{'answer': self.on_success(parsed_items)}}
 
 
-structural = NLPStructuralField(name='structural')
-mep = NLPField(name='MEP', other_names=['mechanical', 'electrical', 'plumbing', 'fire protection'])
-
-
 @dataclass
 class NLPAgent(NLPModelsHelper):
     def __post_init__(self):
-        self.fields = [structural, mep]
+        self.fields = self.setup_fields()
         self.nlp_models = NLPModels()
+
+    def setup_fields(self):
+        structural = NLPStructuralField(name='structural')
+        mep = NLPField(name='MEP', other_names=['mechanical', 'electrical', 'plumbing', 'fire protection'])
+        return [structural, mep]
 
     def process_prompt(self, prompt):
         if not self.zero_shot(prompt, candidate_label='request'):
-            print('Please, a request must be placed.')
-            return None
+            answer = 'Please, a request must be placed.'
+            return {'success': False, 'answer': answer}
         for field in self.fields:
             if field.found_inside(prompt):
                 return field.process_prompt(prompt)
@@ -206,8 +220,14 @@ class NLPAgent(NLPModelsHelper):
 
 
 if __name__ == '__main__':
-    query = 'Please, lower by 0.2 m the height of beam B125'
+    queries = [
+        'Please, lower by 0.2 m the height of beam B125',
+        'Can you move 0.5m to the front a wall W15?',
+        'Displace column C27 one meter to the left, please',
+        'Displace column C27 1 m to the left, please'
+    ]
 
     nlp_agent = NLPAgent()
-    request_data = nlp_agent.process_prompt(query)
-    print(request_data)
+    for query in queries:
+        request_data = nlp_agent.process_prompt(query)
+        print(request_data)
