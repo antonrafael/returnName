18c854f3aa96020917c49d667e6f61255b49d140
antonrafael <anton.rafael10@gmail.com>
2022-05-12 16:11:53 -0300

    Structural Bim Model

---
+++ SpeckleHackathonProjectRevit/Commands/BeamCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SpeckleHackathonProjectRevit.Commands
+{
+    public class BeamCommand
+    {
+        public Guid Id { get; set; }
+        public int ElementId { get; set; }
+
+        public void GetElement()
+        {
+        }
+        public void MoveRight()
+        {          
+        }
+        public void MoveLeft()
+        {
+        }
+    }
+}
